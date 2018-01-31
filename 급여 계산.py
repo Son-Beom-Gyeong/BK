df83968d8a939c4a98ac6b8a36924966e738887c
Son-Beom-Gyeong <35824249+Son-Beom-Gyeong@users.noreply.github.com>
2018-01-31 00:21:10 +0000

    int(input())을 사용한 계산

---
+++ 급여 계산.py
@@ -0,0 +1,7 @@
+
+급여 = int(input("급여를 입력해주세요 : "))
+시간  = int(input("시간을 입력해주세요 : "))
+
+salary = 급여*시간
+print(salary)
+         
---
+++ 화씨 섭씨로 변환.py
@@ -0,0 +1,5 @@
+섭씨 = int(input("섭씨온도를 입력해주세요 : "))
+
+change = 9/5*섭씨 + 32
+
+print(change)         
