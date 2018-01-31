7502526deec99d2f5a8ecc6dc0999547a3508339
Son-Beom-Gyeong <35824249+Son-Beom-Gyeong@users.noreply.github.com>
2018-01-31 07:44:01 +0000

    if문 연습

---
+++ if문 실습.py
@@ -0,0 +1,20 @@
+grade = int(input("학점을 기입하십시오 :"))
+
+if grade >= 95 :        
+            print("A+")
+elif grade >= 90 :
+            print("A")
+elif grade >= 85 :
+            print("B+")
+elif grade >= 80 :
+            print("B")
+elif grade >= 75 :
+            print("c+")
+elif grade >= 70 :
+            print("c")
+elif grade >= 65 :
+            print("d+")
+elif grade >= 60 :
+            print("d")
+else :
+            print("F")
---
+++ if문 연습2.py
@@ -0,0 +1,10 @@
+grade =int(input("점수를 입력하세요 : "))
+
+if grade >= 90 and grade <=100 :
+        print("A등급" )
+elif  grade >= 80 and grade <90 :
+        print("B등급" )
+elif grade >= 70 and grade <80 :
+        print("C등급" )
+else :
+    print("D등급")
