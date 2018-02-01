0ad3a84c5b07980e08cef2b35540391f7a40fef9
Son-Beom-Gyeong <35824249+Son-Beom-Gyeong@users.noreply.github.com>
2018-02-01 00:57:07 +0000

    list의 덧셈과 뺄셈

---
+++ 아침실습 실습2.py
@@ -0,0 +1,10 @@
+spell_re=["r", "e"]
+spell_serve=["s", "e", 'r', 'v','e']
+
+spell_reserve = spell_re + spell_serve
+print(spell_reserve)
+
+
+spell_reserve[0] = "o"
+spell_reserve[1] = "b"
+print(spell_reserve)
