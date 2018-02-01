d8968bce42baf8884c27482e432480e488c32986
Son-Beom-Gyeong <35824249+Son-Beom-Gyeong@users.noreply.github.com>
2018-02-01 00:56:18 +0000

    리스트를 사용한 if문

---
+++ 아침실습.py
@@ -0,0 +1,12 @@
+list_ex1 = ["risk", "issue", "test", "maintenance", "maturity"]
+list_ex2 = ["security", "plan", "design", "systematic", 'safety']
+list_ex3 = ["maintenance", "verification", "validation"]
+
+if ('maintenance' in list_ex1 and len(list_ex1) >= 5) :
+        print("list_ex1을 시험문제로 사용할 수 있습니다")
+
+if ('maintenance' in list_ex2 and len(list_ex2) >= 5) :
+        print("list_ex2을 시험문제로 사용할 수 있습니다.")
+
+if('maintenance' in list_ex3 and len(list_ex3) >= 5) :
+        print("list_ex3을 시험문제로 사용할 수 있습니다.") 
